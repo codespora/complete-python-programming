bbc07e2b267f3c30145f852a62c4ac32e06ddff7
unknown <abdijabarmohamed3@gmail.com>
2021-08-19 15:24:57 +0300

    appending the first comment to a Python file that will contain the content for the conditionals lesson

--- Conditionals/ifelse.py
+++ Conditionals/ifelse.py
@@ -1,4 +1,4 @@
 '''
-Hello and welcome to this class. In today's lesson, we are going to learn about the concept of conditionals of the Python Programming Language. In more simplistic terms, this is known as the if else statements.
+Hello students. Welcome to this class. In today's lesson, we are going to learn about the concept of conditionals of the Python Programming Language. In more simplistic terms, this is known as the if else statements.
 
 '''
