5145fd9b24e0573a25dc650ffbf24f55763ba99e
unknown <abdijabarmohamed3@gmail.com>
2021-08-21 12:20:40 +0300

    incorporating an example in which an if statement is executed and the else part of the code is ignored

--- Conditionals/ifelse.py
+++ Conditionals/ifelse.py
@@ -13,4 +13,22 @@ else:
 
 ''' 
 
-#Let's 
+#Let's start with a simple example of an if else statement
+
+name = "Ali" #here we define a name of a student called
+if name == "Ali":
+	print("My name is Ali and I am a student of Codespora")
+else:
+	print("My name is Halimo and I would like to be a classmate of Ali") 
+
+'''
+As you can see from the above code, since the Boolean expression that follows the if statement evaluates to true, the block statement i.e 
+"My name is Ali and I am a student of Codespora" is executed and hence printed out. The else section of the code is ignored as the if part 
+of the code block evaluates to true. Note that == and = convey a different meaning in programming logic. Whereas == implies an evaluation of 
+equality, the latter, =, is used to assign values to a variable. For instance, 1==1 (means 1 is equal to 1 and thus is true), 1==2 (indicates
+that 1 is equal to 2 and hence false). On the other hand, a=1 (does not mean that a is equal to 1. Rather, the variable a is assigned to the value
+of 1).
+
+''' 
+#In this example, 
+
