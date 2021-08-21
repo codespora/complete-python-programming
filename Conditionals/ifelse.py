0dbd9d536f8d899b41f6f1f81bc59e14c8aae926
unknown <abdijabarmohamed3@gmail.com>
2021-08-21 12:11:13 +0300

    adding detailed descriptions for the if else control flow structure in Python

--- Conditionals/ifelse.py
+++ Conditionals/ifelse.py
@@ -1,4 +1,16 @@
 '''
-Hello students. Welcome to this class. In today's lesson, we are going to learn about the concept of conditionals of the Python Programming Language. In more simplistic terms, this is known as the if else statements.
+Hello students. Welcome to this class. In today's lesson, we are going to learn about the concept of conditionals of the Python 
+Programming Language. In more simplistic terms, this is known as the if else statements. The use of conditionals is of paramount
+importance in programming for the following reason: it provides the programmer with the ability to check conditions and modify
+the program's behavior as deemed fit. The most basic unit of conditionals is the if statement, whose general form is as follows:
+an if followed by a Boolean expression to be evaluated. If the Boolean expression is not evaluated, then the statements under the 
+"else" are evaluated instead. 
 
-'''
+if BOOLEAN EXPRESSION:  
+	STATEMENTS 1
+else:
+	STATEMENTS 2
+
+''' 
+
+#Let's 
